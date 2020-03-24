331eca3c31d60f6c1c033768f4b8f9134284f8a2
erickgimenes <ekgimenes@gmail.com>
2020-03-24 15:38:46 -0300

    Adding AdvertisementComponent to app module.

--- Classificados-Mobile/src/app/app.module.ts
+++ Classificados-Mobile/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { AdvertisementsComponent } from './advertisements/advertisements.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdvertisementsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AdvertisementsComponent]
 })
 export class AppModule { }
--- Classificados-Mobile/src/app/mock-advertisements.ts
+++ Classificados-Mobile/src/app/mock-advertisements.ts
@@ -1,14 +1,12 @@
 import { Advertisement } from './advertisement';
 
 export const ADVERTISEMENTS: Advertisement[] = [
-  { id: 11, title: 'title' },
-  { id: 12, title: 'title' },
-  { id: 13, title: 'title' },
-  { id: 14, title: 'title' },
-  { id: 15, title: 'title' },
-  { id: 16, title: 'title' },
-  { id: 17, title: 'title' },
-  { id: 18, title: 'title' },
-  { id: 19, title: 'title' },
-  { id: 20, title: 'title' }
+  { id: 1, title: 'Vendo chevette 76' },
+  { id: 2, title: 'Vagas para coveiros' },
+  { id: 3, title: 'Compro crack' },
+  { id: 4, title: 'Troco figurinhas de Magic' },
+  { id: 5, title: 'Vendo casal de ganso' },
+  { id: 6, title: 'Vendo botina de pedreiro' },
+  { id: 7, title: 'Compro esterco de vaca' },
+
 ];
